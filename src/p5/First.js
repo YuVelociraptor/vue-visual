b5286ac0d528076c1f8ac2f358211c3538d645a1
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2022-11-30 10:18:55 +0900

    p5表示

---
+++ src/p5/First.js
@@ -0,0 +1,19 @@
+const p5Setup = function (p5){
+
+    p5.setup = () => {
+        const canvas = p5.createCanvas(500, 500);
+        canvas.parent('canvas');
+
+        p5.background(255, 255, 200);
+
+        p5.smooth();
+
+        p5.frameRate(30);
+    };
+
+    p5.draw = () => {
+
+    };
+};
+
+export {p5Setup};
