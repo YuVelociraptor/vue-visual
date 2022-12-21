2f768803f03c75e7fca71ad23b6a2c99757c6879
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2022-12-21 16:37:55 +0900

    3D Line and Rotation

--- src/p5/c60.js
+++ src/p5/c60.js
@@ -3,7 +3,7 @@ const p5Setup = function (p5){
     let i = 0;
 
     p5.setup = () => {
-        const canvas = p5.createCanvas(500, 500);
+        const canvas = p5.createCanvas(500, 500, p5.WEBGL);
         canvas.parent('canvas');
 
         p5.background(255, 255, 200);
@@ -15,8 +15,21 @@ const p5Setup = function (p5){
 
     p5.draw = () => {
 
-        p5.background(p5.abs(p5.sin(p5.radians(i))) * 255, 255, 200);
         i = i + 1;
+        //p5.background(p5.abs(p5.sin(p5.radians(i))) * 255, 255, 200);
+        p5.background(255, 255, 200);
+
+        p5.rotateX(p5.radians(i));
+
+        // p5.line(-100, -100, 100 - i, -100, 100, 100 - i);
+        // p5.line(-100, 100, 100 - i, 100, 100, 100 - i);
+        // p5.line(100, 100, 100 - i, 100, -100, 100 - i);
+        // p5.line(100, -100, 100 - i, -100, -100, 100 - i);
+
+        p5.line(-100, -100, 0, -100, 100, 0);
+        p5.line(-100, 100, 0, 100, 100, 0);
+        p5.line(100, 100, 0, 100, -100, 0);
+        p5.line(100, -100, 0, -100, -100, 0);
     };
 };
 
