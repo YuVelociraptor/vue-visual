b1afbc6031d3657e012416a91e10ee0fede9dac2
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2023-01-04 10:34:44 +0900

    ライフゲーム作成

---
+++ src/components/GemeOfLife.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="canvas"></div>
+  </div>
+</template>
+
+<script>
+
+import { onMounted, ref } from 'vue';
+import p5 from 'p5';
+import { p5Setup } from '../p5/c60';
+
+
+export default {
+
+  setup() {
+    const P5 = ref();
+
+    // マウント時に canvas を生成
+    onMounted(() => {
+      P5.value = new p5(p5Setup);
+    });
+
+    return {
+      P5
+    };
+  }
+
+};
+</script>
