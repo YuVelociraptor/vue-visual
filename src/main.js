f6173adea83bff55471a146d1ee98b06c5429567
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2023-01-05 17:13:54 +0900

    gtag使用

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import router from './router/index'
 import App from './App.vue'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
+import VueGtag from "vue-gtag";
 
-createApp(App).use(Quasar, quasarUserOptions).use(router).mount('#app')
+createApp(App)
+    .use(Quasar, quasarUserOptions)
+    .use(router)
+    .use(VueGtag, {
+        config: { id: process.env.VUE_APP_GA_ID }
+    })
+    .mount('#app')
